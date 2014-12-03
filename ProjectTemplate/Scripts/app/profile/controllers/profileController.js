a6f099d0a4f95eda4bde8832d41405634de8bbd4
Tony Maddox <tony.maddox@ingagepartners.com>
2014-12-03 13:36:32 -0500

    message

--- ProjectTemplate/Scripts/app/profile/controllers/profileController.js
+++
@@ -1,9 +0,0 @@
-﻿angular.module('crdsProfile').controller('crdsProfileCtrl', [
-    '$scope', 'Profile',
-    function ($scope, Profile) {
-
-        Profile.get().then(function (data) {
-            $scope.person = data;
-        });
-    }
-]);
